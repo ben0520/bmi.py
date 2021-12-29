2840b30db352fefa788e3fc20da758c5750f3c8d
ben0520 <95621349+ben0520@users.noreply.github.com>
2021-12-30 05:11:22 +0800

    Update 456.py

--- 456.py
+++ 456.py
@@ -32,13 +32,55 @@ if inpt == ans+1:
         
 else:
     st.write('错误，正确答案为：',ans+1,choice[ans])
-        
-        
-        
 
-inpt = st.text_input('Please input 1, 2, 3, 4', '0')
+a = st.text_input('請輸入答案？', '0')
+b = st.text_input('請輸入答案？', '0')
+c = st.text_input('請輸入答案？', '0')
+d = st.text_input('請輸入答案？', '0')
+e = st.text_input('請輸入答案？', '0')
+f = st.text_input('請輸入答案？', '0')
+g = st.text_input('請輸入答案？', '0')
+h = st.text_input('請輸入答案？', '0')
+i = st.text_input('請輸入答案？', '0')
+j = st.text_input('請輸入答案？', '0')
+k = st.text_input('請輸入答案？', '0')
+l = st.text_input('請輸入答案？', '0')
+m = st.text_input('請輸入答案？', '0')
+n = st.text_input('請輸入答案？', '0')
+o = st.text_input('請輸入答案？', '0')
+p = st.text_input('請輸入答案？', '0')
+q = st.text_input('請輸入答案？', '0')
+r = st.text_input('請輸入答案？', '0')
+s = st.text_input('請輸入答案？', '0')
+t = st.text_input('請輸入答案？', '0')
+u = st.text_input('請輸入答案？', '0')
+v = st.text_input('請輸入答案？', '0')
+w = st.text_input('請輸入答案？', '0')
+x = st.text_input('請輸入答案？', '0')
+y = st.text_input('請輸入答案？', '0')
+z = st.text_input('請輸入答案？', '0')
+A = st.text_input('請輸入答案？', '0')
+B = st.text_input('請輸入答案？', '0')
+C = st.text_input('請輸入答案？', '0')
+D = st.text_input('請輸入答案？', '0')
+E = st.text_input('請輸入答案？', '0')
+F = st.text_input('請輸入答案？', '0')
+G = st.text_input('請輸入答案？', '0')
+H = st.text_input('請輸入答案？', '0')
+I = st.text_input('請輸入答案？', '0')
+J = st.text_input('請輸入答案？', '0')
+K = st.text_input('請輸入答案？', '0')
+L = st.text_input('請輸入答案？', '0')
+M = st.text_input('請輸入答案？', '0')
+N = st.text_input('請輸入答案？', '0')
+O = st.text_input('請輸入答案？', '0')
+P = st.text_input('請輸入答案？', '0')
+Q = st.text_input('請輸入答案？', '0')
+R = st.text_input('請輸入答案？', '0')
+S = st.text_input('請輸入答案？', '0')
+T = st.text_input('請輸入答案？', '0')
 comfirm_input3 = st.button('輸入確認')
-if comfirm_input3:
-    comfirm_input3=0        
+if comfirm_input:
+    comfirm_input=0        
 
         
