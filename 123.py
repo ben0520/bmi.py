ef2e69baef26d1c4e577e3c97077e122f631ffc6
ben0520 <95621349+ben0520@users.noreply.github.com>
2021-12-30 02:32:30 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -19,31 +19,27 @@ for k,v in train.items():
         ans = random.randint(0,3)
         choice[ans] = v
         st.write('1:',choice[0],'   2:',choice[1],'   3:',choice[2],'   4:',choice[3])
-        inpt = st.text_input('Please input 1, 2, 3, 4', '0')
-        comfirm_input = st.button('確認')
-        if comfirm_input:
-            comfirm_input=0
-            while inpt not in ['1','2','3','4']:
-                st.write(输入错误)
-                inpt = st.text_input('Please input 1, 2, 3, 4', '0')
-                comfirm_input2 = st.button('輸入確認')
-                if comfirm_input2:
-                    comfirm_input2=0
-                inpt = int(inpt)
-            if inpt == ans+1:
-                st.write('正确')
-                count += 1;
-            else:
-                st.write('错误，正确答案为：',ans+1,choice[ans])
+        while inpt not in ['1','2','3','4']:
+            st.write(输入错误)
+            inpt = st.text_input('Please input 1, 2, 3, 4', '0')
+            comfirm_input2 = st.button('輸入確認')
+            if comfirm_input2:
+                comfirm_input2=0
+            inpt = int(inpt)
+        if inpt == ans+1:
+            st.write('正确')
+            count += 1;
+        else:
+            st.write('错误，正确答案为：',ans+1,choice[ans])
 
-            finish = time.time()
-            st.write('\n恭喜，完成一次训练，正确率：',count,'/',46)
-            st.write('用时：%.2f 秒\n' % (finish-start))
+        finish = time.time()
+        st.write('\n恭喜，完成一次训练，正确率：',count,'/',46)
+        st.write('用时：%.2f 秒\n' % (finish-start))
 
-            with open (filename,'a') as f:
-                f.write(time.strftime('%Y-%m-%d-%H %I:%M',tim1))
-                f.write('\n     正确率：')
-                f.write(str(count))
-                f.write('/')
-                f.write(str(46))
-                f.write('    用时：%.2f 秒\n' % (finish-start))
+        with open (filename,'a') as f:
+            f.write(time.strftime('%Y-%m-%d-%H %I:%M',tim1))
+            f.write('\n     正确率：')
+            f.write(str(count))
+            f.write('/')
+            f.write(str(46))
+            f.write('    用时：%.2f 秒\n' % (finish-start))
