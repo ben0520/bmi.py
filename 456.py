1a985907342e3f5d75c675c0edd3cda51756d586
ben0520 <95621349+ben0520@users.noreply.github.com>
2021-12-30 04:59:36 +0800

    Update 456.py

--- 456.py
+++ 456.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import random
 import time
-ping = ['あ', 'い', 'う', 'え', 'お', 'か', 'き', 'く', 'け', 'こ', 'さ', 'し', 'す', 'せ', 'そ', 'た', 'ち', 'つ', 'て', 'と', 'な', 'に', 'ぬ', 'ね', 'の', 'は', 'ひ', 'ふ', 'へ', 'ほ', 'ま', 'み', 'む', 'め', 'も', 'や', 'ゆ', 'よ', 'ら', 'り', 'る', 'れ', 'ろ', 'わ', 'を', 'ん']
+ping = ['a あ', 'b い', 'c う', 'd え', 'e お', 'f か', 'g き', 'h く', 'i け', 'j こ', 'k さ', 'l し', 'm す', 'n せ', 'o そ', 'p た', 'q ち', 'r つ', 's て', 't と', 'u な', 'v に', 'w ぬ', 'x ね', 'y の', 'z は', 'A ひ', 'B ふ', 'C へ', 'D ほ', 'E ま', 'F み', 'G む', 'H め', 'I も', 'J や', 'K ゆ', 'L よ', 'M ら', 'N り', 'O る', 'P れ', 'Q ろ', 'R わ', 'S を', 'T ん']
 yin = ['a', 'i', 'u', 'e', 'o', 'ka', 'ki', 'ku', 'ke', 'ko', 'sa', 'si/shi', 'su', 'se', 'so', 'ta', 'ti/chi', 'tu/tsu', 'te', 'to', 'na', 'ni', 'nu', 'ne', 'no', 'ha', 'hi', 'hu/fu', 'he', 'ho', 'ma', 'mi', 'mu', 'me', 'mo', 'ya', 'yu', 'yo', 'ra', 'ri', 'ru', 're', 'ro', 'wa', 'wo','n']
+
+h = st.text_input('請輸入身高(M)？', '168')
 
 filename = './训练记录.log'
 tim1 = time.localtime(time.time())
@@ -38,5 +40,5 @@ inpt = st.text_input('Please input 1, 2, 3, 4', '0')
 comfirm_input3 = st.button('輸入確認')
 if comfirm_input3:
     comfirm_input3=0        
-answer = st.select_slider('Select a answer of the qs',options=['1', '2', '3', '4'])
+
         
