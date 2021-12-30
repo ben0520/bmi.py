0165f57a5f34f99dd357825fe9b8e7f36b0044a7
ben0520 <95621349+ben0520@users.noreply.github.com>
2021-12-30 13:46:51 +0800

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,101 +1,90 @@
-import pandas as pd
+import streamlit as st
+import plotly.graph_objs as go
+from stock import stock
+#stock是我自己寫的一個小小的股票爬蟲，有需要可以去github上面看
 from datetime import datetime
-from dateutil.relativedelta import relativedelta
+from plotly.subplots import make_subplots
 
-class stock:
-    now = datetime.now()
-    def __init__(self, stock_code, from_month) -> None:
-        self.stock_code = stock_code
-        self.from_month = from_month
-        if self.now.month< from_month:
-            self.start = datetime(self.now.year-1, self.from_month, 1)
-        else:
-            self.start = datetime(self.now.year, self.from_month, 1)
+#利用st.cache()快取沒有改變過的data
+@st.cache()
+# 爬蟲程式
+def get_stock_data(stock_code, month):
+    #stock_code就是股票代碼，month就是月份，看要抓幾月到現在的月份
+    return stock(stock_code, month).get_all_data()
+    #會返回一個DF
 
-    def get_three_major(self): 
-        """
-        	日期	外資	投信	自營商	單日合計
-        0	109/12/04	18731	77	-340	18468
-        1	109/12/03	-4885	13	-57	-4929
-        2	109/12/02	10469	106	-415	10160
-        3	109/12/01	587	211	397	1195
-        4	109/11/30	-33427	-42	1226	-32243
-        
-        """
 
-        end = self.now
-        time_format = "%Y-%m-%d"
-        start_string = self.start.strftime(time_format)
-        end_string = end.strftime(time_format)
-        # 日盛證卷
-        url = f'http://jsjustweb.jihsun.com.tw/z/zc/zcl/zcl.djhtm?a={self.stock_code}&d={end_string}&c={start_string}'
-        df_orignal = pd.read_html(url)
-        df_extracted = df_orignal[2][7:-1]
-
-        #如果沒有資料回傳，則 回傳錯誤
-        if len(df_extracted.index) < 2:
-            raise Exception('No data retrun from this stock code')
-        #設定df columns 不然都會是1.2.3......
-        df_extracted.columns = df_orignal[2].iloc[6]
-        df_extracted.columns.name = ''
-
-        return df_extracted.reset_index(drop = True).iloc[:,:5]
-
-    def get_price_value(self):
-        """
-        
-        1	日期	開盤價	最高價	最低價	收盤價	成交量
-        0	109/06/30	313.50	314.00	311.00	313.00	48784
-        1	109/06/29	314.00	315.00	310.00	312.00	56455
-        2	109/06/24	319.00	320.00	316.00	317.50	54019
-        3	109/06/23	316.00	316.50	312.50	315.00	41183
-        4	109/06/22	314.50	316.50	312.00	312.00	37295
-        
-        
-        """
-        data_list = []
-        query_time = self.start
-        while (query_time<self.now):
-            #聚財網
-            url = f'https://stock.wearn.com/cdata.asp?Year={query_time.year-1911}&month={query_time.month:0>2d}&kind={self.stock_code}'
-            df_orignal =  pd.read_html(url)
-            df_extracted = df_orignal[0][2:]
-            df_extracted.columns = df_orignal[0].iloc[1]
-            data_list.append(df_extracted)
-            query_time += relativedelta(months = +1)
-        data_final = pd.concat(data_list).drop_duplicates().reset_index(drop = True)
-        data_final.columns.name = ''
-        return data_final
-
-    def get_all_data(self, enable_color = True):
-        """
-        	日期	外資	投信	自營商	單日合計	開盤價	最高價	最低價	收盤價	成交量
-        0	109/12/04	18731	77	-340	18468	498.50	505.00	497.50	503.00	50920
-        1	109/12/03	-4885	13	-57	-4929	499.50	499.50	495.00	497.00	35557
-        2	109/12/02	10469	106	-415	10160	499.50	500.00	493.50	499.00	50422
-        3	109/12/01	587	211	397	1195	489.50	490.00	483.50	490.00	36915
-        """
-        data1 = self.get_three_major()
-        data2 = self.get_price_value()
-        data = data1.merge(data2, left_on = '日期', right_on = '日期')
-
-        # 將日期109/11/1 轉換 至2020/11/1...
-        data['日期'] = data['日期'].apply(lambda x: f'{int((x :=x.split("/"))[0])+1911}/{x[1]}/{x[2]}')
+# ---------------------------------- sidebar --------------------------------- #
 
 
 
-        # 屬性轉換 --> 將其餘資料轉至 number
-        for i in data.columns:
-            data[i] = pd.to_numeric(data[i], errors='ignore')
+st.sidebar.header('Parameter setting')
+stock_code = st.sidebar.text_input('pls input the stock code')
+month = st.sidebar.number_input('from month...',
+                                value=datetime.now().month - 3,
+                                step=1,
+                                max_value=12,
+                                min_value=1)
+data_colums = ['外資', '自營商', '投信', '成交量']
+Bubble_info = st.sidebar.selectbox('Bubble_info', data_colums)
+Bubble_size = st.sidebar.number_input('Bubble_size', step=1, value=10)
+sub_info = st.sidebar.selectbox('Sub_info', data_colums)
 
-        # 屬性轉換 --> 將日期從object to datetime
-        data['日期'] = data['日期'].astype('datetime64[ns]')
+# ----------------------------------- body ----------------------------------- #
+st.image('./icon.png')
+st.title('Stock chart')
 
-        if enable_color:
-            data['外資買賣顏色'] = data['外資'].apply(lambda x : '#FF0000' if x>0 else '#008E09' )
-            data['投信買賣顏色'] =  data['投信'].apply(lambda x : '#FF0000' if x>0 else '#008E09' )
-            data['自營商買賣顏色'] =  data['自營商'].apply(lambda x : '#FF0000' if x>0 else '#008E09' )
-            data['收盤價買賣顏色'] =  (data['收盤價']-data['開盤價']).apply(lambda x : '#FF0000' if x>0 else '#008E09' )
-            data['成交量買賣顏色'] =  (data['收盤價']-data['開盤價']).apply(lambda x : '#FF0000' if x>0 else '#008E09' )
+# ----------------------------------- plotly --------------------------------- #
 
-        return data
+if stock_code and month:
+    #如果stock_code與month都有值的話，則畫圖
+    data = get_stock_data(stock_code, month)
+    st.dataframe(data)
+    st.success('Load data success !')
+
+    if Bubble_info != '成交量':
+        #如果選項不同，畫圖則不同
+        trace1 = go.Scatter(
+            x=data['日期'],
+            y=data['收盤價'],
+            mode='lines+markers',
+            marker=dict(size=data[f'{Bubble_info}'].abs(),
+                        sizeref=data[f'{Bubble_info}'].abs().mean() /
+                        Bubble_size,
+                        color=data[f'{Bubble_info}買賣顏色']),
+            line =dict(dash= 'dot'),
+            hovertemplate="<b>日期%{x}</b><br> 收盤價 %{y} " + f"{Bubble_info} :" +
+            "%{marker.size}<br>",
+            name='收盤價')
+    else:
+        trace1 = go.Scatter(x=data['日期'],
+                            y=data['收盤價'],
+                            mode='lines+markers',
+                            marker=dict(
+                                size=data[f'{Bubble_info}'].abs(),
+                                sizeref=data[f'{Bubble_info}'].abs().mean() /
+                                Bubble_size,
+                            ),
+                            line =dict(dash= 'dot'),
+                             hovertemplate="<b>日期%{x}</b><br> 收盤價 %{y}",
+                            name='收盤價')
+
+
+    trace2 = go.Bar(x=data['日期'],
+                    y=data[f'{sub_info}'],
+                    name=f'{sub_info}',
+                    marker_color=data[f'{sub_info}買賣顏色'])
+
+    
+    fig = make_subplots(rows=2,
+                        cols=1,
+                        shared_xaxes=True,
+                        row_heights=[0.7, 0.3])
+    fig.add_trace(trace1, row=1, col=1)
+    fig.add_trace(trace2, row=2, col=1)
+
+    fig.update_layout(title=f'{stock_code}_chart', template='plotly_dark')
+
+
+    st.plotly_chart(fig)
+    #st.繪圖呈現
