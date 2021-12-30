da5fad0f3b907822aae2997d8df7fd499e6ac916
ben0520 <95621349+ben0520@users.noreply.github.com>
2021-12-30 14:42:07 +0800

    Update 456.py

--- 456.py
+++ 456.py
@@ -2,17 +2,19 @@ import streamlit as st
 import random
 dict = {'あ':'a', 'い':'i', 'う':'u', 'え':'e', 'お':'o', 'か':'ka', 'き':'ki', 'く':'ku', 'け':'ke', 'こ':'ko', 'さ':'sa', 'し':'si/shi', 'す':'su', 'せ':'se', 'そ':'so', 'た':'ta', 'ち':'ti/chi', 'つ':'tu/tsu', 'て':'te', 'と':'to', 'な':'na', 'に':'ni', 'ぬ':'nu', 'ね':'ne', 'の':'no', 'は':'ha', 'ひ':'hi', 'ふ':'hu/fu', 'へ':'he', 'ほ':'ho', 'ま':'ma', 'み':'mi', 'む':'mu', 'め':'me', 'も':'mo', 'や':'ya', 'ゆ':'yu', 'よ':'yo', 'ら':'ra', 'り':'ri', 'る':'ru', 'れ':'re', 'ろ':'ro', 'わ':'wa', 'を':'wo', 'ん':'n'}
 def state():
-    return random.choice(list(d.keys()))
-    st.write(state(),'的读音：')
-
-    for key, value in dict.items():
-        answer = st.text_input('請輸入答案(a)？', '0')
-        comfirm_input = st.button('輸入確認')
-        if comfirm_input:
-            if value == answer:
-                print('正确')
-            else:
-                print('错误')
+    return random.choice(list(dict))
+    value=state()
+    st.write(value,'的读音：')
+    ans=dict[value]
+    answer = st.text_input('Please input 'a,i,u,e,o,ka,ki,ku,ke,ko,sa,si/shi,su,se,so,ta,ti/chi,tu/tsu,te,to,na,ni,nu,ne,no,ha,hi,hu/fu,he,ho,ma,mi,mu,me,mo,ya,yu,yo,ra,ri,ru,re,ro,wa,wo,n', '0')
+    comfirm_input = st.button('確認')
+    if comfirm_input:
+        if answer == ans :
+            print('正确')
+        else:
+            print('错误')
+                
+                
 
         
         
