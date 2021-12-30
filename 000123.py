374cdc34902cf68464349681fae7786014a6a560
ben0520 <95621349+ben0520@users.noreply.github.com>
2021-12-30 12:58:05 +0800

    Update 000123.py

--- 000123.py
+++ 000123.py
@@ -4,5 +4,13 @@ a = st.text_input('請輸入答案(a)？', '0')
 b = st.number_input('請輸入答案？(b)', '0')
 comfirm_input = st.button('輸入確認')
 if comfirm_input:
+    if inpt == ans+1:
+        print('正确')
+        count += 1;
+    else:
+        print('错误，正确答案为：',ans+1,choice[ans])
 
-
+        
+        
+        
+        
