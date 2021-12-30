3dd75a5a5c71d38bfc265e2a8e2e1900e4a6e47d
ben0520 <95621349+ben0520@users.noreply.github.com>
2021-12-30 14:53:11 +0800

    Update 456.py

--- 456.py
+++ 456.py
@@ -3,11 +3,11 @@ import random
 dict = {'あ':'a', 'い':'i', 'う':'u', 'え':'e', 'お':'o', 'か':'ka', 'き':'ki', 'く':'ku', 'け':'ke', 'こ':'ko', 'さ':'sa', 'し':'si/shi', 'す':'su', 'せ':'se', 'そ':'so', 'た':'ta', 'ち':'ti/chi', 'つ':'tu/tsu', 'て':'te', 'と':'to', 'な':'na', 'に':'ni', 'ぬ':'nu', 'ね':'ne', 'の':'no', 'は':'ha', 'ひ':'hi', 'ふ':'hu/fu', 'へ':'he', 'ほ':'ho', 'ま':'ma', 'み':'mi', 'む':'mu', 'め':'me', 'も':'mo', 'や':'ya', 'ゆ':'yu', 'よ':'yo', 'ら':'ra', 'り':'ri', 'る':'ru', 'れ':'re', 'ろ':'ro', 'わ':'wa', 'を':'wo', 'ん':'n'}
 def state():
     return random.choice(list(dict))
-    value=state()
-    st.write(value,'的读音：')
-    ans=dict[value]
-    answer = st.text_input('Please input 音標', 'a')
-    comfirm_input = st.button('確認')
+value=state()
+st.write(value,'的读音：')
+ans=dict[value]
+answer = st.text_input('Please input 音標', 'a')
+comfirm_input = st.button('確認')
     if comfirm_input:
         if answer == ans :
             print('正确')
