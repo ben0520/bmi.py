c11cc8b9649f1ea5f7b7d6260395466faba4b359
ben0520 <95621349+ben0520@users.noreply.github.com>
2022-01-03 19:37:00 +0800

    Update 456.py

--- 456.py
+++ 456.py
@@ -3,21 +3,20 @@ import random
 dict = {'あ':'a', 'い':'i', 'う':'u', 'え':'e', 'お':'o', 'か':'ka', 'き':'ki', 'く':'ku', 'け':'ke', 'こ':'ko', 'さ':'sa', 'し':'si/shi', 'す':'su', 'せ':'se', 'そ':'so', 'た':'ta', 'ち':'ti/chi', 'つ':'tu/tsu', 'て':'te', 'と':'to', 'な':'na', 'に':'ni', 'ぬ':'nu', 'ね':'ne', 'の':'no', 'は':'ha', 'ひ':'hi', 'ふ':'hu/fu', 'へ':'he', 'ほ':'ho', 'ま':'ma', 'み':'mi', 'む':'mu', 'め':'me', 'も':'mo', 'や':'ya', 'ゆ':'yu', 'よ':'yo', 'ら':'ra', 'り':'ri', 'る':'ru', 'れ':'re', 'ろ':'ro', 'わ':'wa', 'を':'wo', 'ん':'n'}
 def state():
     return random.choice(list(dict))
-value=state()
-st.write(value,'的读音：')
 
 submit_button = st.button(label='開始遊戲')    
 if submit_button: 
-     for i in dict: 
-         value+=str(dict[i]) 
-     st.session_state.answer=value 
+    value=state()
+    st.session_state.answer=value 
 
-ans=st.sidebar.text_input('Please input 音標', 'a')     
+    st.write(st.session_state.answer,'的读音：')
+    ans=dict[st.session_state.answer]
+    ans=st.sidebar.text_input('Please input 音標', 'a')   
      
-if ans==st.session_state.answer:
-    st.write('正确')
-else:
-    st.write('错误')
+    if ans==st.session_state.answer:
+        st.write('正确')
+    else:
+        st.write('错误')
             
             
  
