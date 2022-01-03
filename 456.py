3302d2d6e189832ced773ef1e1ebb503fc5680dc
ben0520 <95621349+ben0520@users.noreply.github.com>
2022-01-03 19:00:59 +0800

    Update 456.py

--- 456.py
+++ 456.py
@@ -5,18 +5,19 @@ def state():
     return random.choice(list(dict))
 value=state()
 st.write(value,'的读音：')
-ans=st.text_input('Please input 音標', 'a')
+
 submit_button = st.button(label='開始遊戲')    
 if submit_button: 
      for i in dict: 
          value+=str(dict[i]) 
      st.session_state.answer=value 
+
+ans=st.sidebar.text_input('Please input 音標', 'a')     
      
-     
-     if ans==st.session_state.answer:
-         st.write('正确')
-     else:
-         st.write('错误')
+if ans==st.session_state.answer:
+    st.write('正确')
+else:
+    st.write('错误')
             
             
  
